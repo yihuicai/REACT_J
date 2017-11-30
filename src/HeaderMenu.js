f1013e566a777a973cdba2694635f997c6246cdd
Alan <yihuicai@buffalo.edu>
2017-11-29 17:25:54 -0800

    TEST

---
+++ src/HeaderMenu.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+export default class HeaderMenu extends Component {
+    render() {
+        return (
+        <div>
+            <img src="../images/header_icon_0_2.png" alt=""/>
+            <a href="/"><img src="https://jinglimited.com/u_file/1711/photo/6f01b193fb.png" alt="" /></a>
+        </div>);
+    }
+}
